4974d14ec1e1c421a0548fa6c6781f9a9aa85ffc
Daniel T14s <121113019+DaniLucky06@users.noreply.github.com>
2026-04-20 20:03:06 +0200

    Python script per centro di massa, tabella masse

---
+++ centro_di_massa.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+N = int(input("Numero di masse: "))
+file = np.loadtxt("masse.csv", delimiter=",")
+
+massa_pirulo = 1.2 # g
+h_pirulo = 6.5 # mm
+l_filo = 850 # mm
+l_voluta = 859.5233598409543 # mm
+
+masse = np.empty(N)
+h = np.empty(N)
+h_tot = 0
+for i in range(N):
+    indice_massa = int(input(f"Massa {i + 1}: ")) - 1
+    masse[i] = file[0,indice_massa]
+    h_i = file[1,indice_massa]
+    h[i] = h_tot + h_i/2
+    h_tot += h_i
+
+h_mean = (sum(masse * h) + massa_pirulo * (h_tot + h_pirulo/2)) / (sum(masse) + massa_pirulo)
+h_top = h_tot - h_mean
+h_voluta = l_voluta - h_top - h_pirulo
+print("Altezza dalla cima (no tappo): ", h_top)
+print("Altezza perno-cima della pila: ", h_voluta + h_pirulo)
+print("Altezza perno-pirulo: ", h_voluta)
