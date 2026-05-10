28e1a5a00cb0ab550c19b61093bd838f49ba8a0c
Daniel ATRAX <121113019+DaniLucky06@users.noreply.github.com>
2026-05-10 20:52:09 +0200

    Corretto calcoli analisi_con_libreria.py - ATRAX

--- Python/analisi_2_con_libreria.py
+++ Python/analisi_2_con_libreria.py
@@ -4,59 +4,55 @@ sys.path.append(os.path.join(dir, "analisi-dati-python"))
 from analysis import *
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy import stats
 
 df = np.loadtxt(os.path.join(dir, '../dati_pendolo_semplice_2.csv'), delimiter=',')
+
+chi2_valido = 1.2
 
 df = df[:, np.argsort(df[0])]
 lunghezze = np.log(df[0] / 100)
 omega = df[1]
-fps = 60
-ris = 0.07 / fps
-err_ris = ris / np.sqrt(12)
 
 periodi = 2 * np.pi / omega
 dev_std_omega = df[2]
-dev_std_periodi = 2 * np.pi / omega**2 * dev_std_omega
-dev_std_periodi = np.abs(1 / periodi) * np.sqrt(dev_std_periodi**2 + err_ris**2)
+
+dev_std_periodi = dev_std_omega / omega
+
 periodi = np.log(periodi)
 
-err_lunghezze = 0.001 / np.sqrt(12)
-dev_std_lunghezze = np.ones(len(lunghezze)) * err_lunghezze
+err_filo = 0.001 / np.sqrt(12)
+err_baricentro = 0.001 / np.sqrt(12)
+err_lunghezze = np.sqrt(err_filo**2 + err_baricentro**2)
+dev_std_lunghezze = np.ones(len(lunghezze)) * err_lunghezze / (df[0] / 100)
 
 measurements = WeightedMeasurements(lunghezze, periodi, dev_std_lunghezze, dev_std_periodi)
 reg_data, xy_data = weightedLinReg(measurements)
 a = reg_data.a
 b = reg_data.b
 
-# plotGraphs([measurements, xy_data], GraphVisuals(['rx','b']))
-
 measurements.y = measurements.y - (a + measurements.x * b)
 xy_data.y = xy_data.y - (a + xy_data.x * b)
 
-
-# plt.figure()
-index=0
-endIndex = -1
+endIndex = None
 reg_data_abs, _ = weightedLinReg(WeightedMeasurements(lunghezze[0:endIndex], periodi[0:endIndex], dev_std_lunghezze[0:endIndex], dev_std_periodi[0:endIndex]))
 
+index = 0
 x = np.linspace(lunghezze[0], lunghezze[-1], 10)
-# plt.plot(x, reg_data.a + reg_data.b * x)
 print(reg_data.chi2, reg_data.nu)
 for i in range(1, len(periodi) - 5):
     reg_data, _ = weightedLinReg(WeightedMeasurements(lunghezze[i:endIndex], periodi[i:endIndex], dev_std_lunghezze[i:endIndex], dev_std_periodi[i:endIndex]))
-    valido = reg_data.chi2 / reg_data.nu < 1.2
+    valido = reg_data.chi2 / reg_data.nu < chi2_valido
     style = 'b' if valido else 'r'
-    print(reg_data.chi2, reg_data.nu, "Valido: ", valido)
+    print(reg_data.chi2, reg_data.nu, reg_data.chi2 / reg_data.nu, "Valido:", valido)
     if valido:
-        # plt.plot(x, reg_data.a + reg_data.b * x - (reg_data_abs.a + reg_data_abs.b * x), style)
         index = i
         break
-# plt.errorbar(lunghezze, periodi - (reg_data_abs.a + reg_data_abs.b * lunghezze), dev_std_periodi, dev_std_lunghezze, 'kx')
-# plt.show()
+
+dev_std_effettiva = np.sqrt(dev_std_periodi**2 + (reg_data.b * dev_std_lunghezze)**2)
 
 plt.figure()
 plt.hlines(0, x[0], x[-1], 'b')
-plt.errorbar(lunghezze[index:-1], periodi[index:-1] - (reg_data.a + reg_data.b * lunghezze[index:-1]), dev_std_periodi[index:-1], dev_std_lunghezze[index:-1], 'gx')
-plt.errorbar(lunghezze[0:index], periodi[0:index] - (reg_data.a + reg_data.b * lunghezze[0:index]), dev_std_periodi[0:index], dev_std_lunghezze[0:index], 'rx')
+
+plt.errorbar(lunghezze[index:-1], periodi[index:-1] - (reg_data.a + reg_data.b * lunghezze[index:-1]), yerr=dev_std_effettiva[index:-1], fmt='gx')
+plt.errorbar(lunghezze[0:index], periodi[0:index] - (reg_data.a + reg_data.b * lunghezze[0:index]), yerr=dev_std_effettiva[0:index], fmt='rx')
 plt.show()
--- Python/analisi_2_scipy.py
+++ Python/analisi_2_scipy.py
@@ -9,36 +9,84 @@ import scipy.optimize as opt
 df = np.loadtxt(os.path.join(dir, '../dati_pendolo_semplice_2.csv'), delimiter=',')
 
 fps = 60
-ris = 0.07 / fps
+ris = 1 / fps
 err_ris_t = ris / np.sqrt(12)
+err_y_intrinsic = 0.0001 # for zero division
+
+chi2_valido = 1.2
 
 def f(t, a, b, c, d, e):
     return a + b * np.exp(c * t) * np.sin(d * t + e)
-
-filePaths = [os.path.join(dir, f) for f in os.listdir(os.path.join(dir, "../videoData")) if os.path.isfile(os.path.join(dir, f))]
+video_dir = os.path.join(dir, "../videoData")
+filePaths = [os.path.join(video_dir, f) for f in os.listdir(video_dir) if os.path.isfile(os.path.join(video_dir, f))]
 omegas = []
-for i, fPath in filePaths:
+errs = []
+
+for i, fPath in enumerate(filePaths):
     data = np.loadtxt(fPath)
     t = data[:, 0]
     y = data[:, 1]
-    a, b, c, d, e = opt.curve_fit(f, t, y)
-    err_y = err_ris_t * np.abs(b * d * np.cos(d * t + e))
-    a, b, c, d, e = opt.curve_fit(f, t, y, sigma=err_y, absolute_sigma=True)
-    omegas.append(d)
-print(omegas)
+
+    a_guess = np.mean(y)
+    b_guess = (np.max(y) - np.min(y)) / 2.
+    c_guess = -.1
+
+    # stima la pulsazione
+    zero_intercepts = np.where(np.diff(np.sign(y - a_guess)))[0] # trova le intersezioni con lo zero
+    period_guess = 2 * np.mean(np.diff(t[zero_intercepts])) # media del tempo fra intersezioni
+    d_guess = 2 * np.pi / period_guess
+
+    p0 = [a_guess, b_guess, c_guess, d_guess, 0.0]
+
+    popt, _ = opt.curve_fit(f, t, y, p0)
+    a, b, c, d, e = popt
+
+    dy_dt = b * np.exp(c * t) * (c * np.sin(d * t + e) + d * np.cos(d * t + e))
+    err_y = np.sqrt(err_y_intrinsic**2 + (err_ris_t * dy_dt)**2)
+
+    popt, pcov  = opt.curve_fit(f, t, y, p0=popt, sigma=err_y, absolute_sigma=True)
+
+    omegas.append(popt[3])
+    err = np.sqrt(np.diag(pcov))[3]
+    errs.append(err)
+
+    """
+    plt.figure()
+    plt.plot(t, y)
+    plt.plot(t, f(t, a, b, c, d, e))
+    plt.title(fPath)
+    plt.show()
+    """
+
+omegas = np.array(omegas)
+errs = np.array(errs)
+
 
 df = df[:, np.argsort(df[0])]
 lunghezze = np.log(df[0] / 100)
 omega = df[1]
 
+indices = np.argsort(omegas)
+for i, j in enumerate(indices[::-1]):
+    print(f"Omegas[j]: {omegas[j]}, omega[i]: {omega[i]}, diff: {omegas[j] - omega[i]}")
+
+# omega = omegas[indices[::-1]]
+
 periodi = 2 * np.pi / omega
 dev_std_omega = df[2]
-dev_std_periodi = 2 * np.pi / omega**2 * dev_std_omega
-dev_std_periodi = np.abs(1 / periodi) * np.sqrt(dev_std_periodi**2 + err_ris_t**2)
+# dev_std_omega = errs[indices[::-1]]
+
+# dev_std_periodi = 2 * np.pi / omega**2 * dev_std_omega
+# dev_std_periodi = np.abs(1 / periodi) * np.sqrt(dev_std_periodi**2 + err_ris_t**2)
+dev_std_periodi = dev_std_omega / omega
+
+
 periodi = np.log(periodi)
 
-err_lunghezze = 0.001 / np.sqrt(12)
-dev_std_lunghezze = np.ones(len(lunghezze)) * err_lunghezze
+err_filo = 0.001 / np.sqrt(12)
+err_baricentro = 0.001 / np.sqrt(12)
+err_lunghezze = np.sqrt(err_filo**2 + err_baricentro**2)
+dev_std_lunghezze = np.ones(len(lunghezze)) * err_lunghezze / (df[0] / 100)
 
 measurements = WeightedMeasurements(lunghezze, periodi, dev_std_lunghezze, dev_std_periodi)
 reg_data, xy_data = weightedLinReg(measurements)
@@ -52,17 +100,18 @@ xy_data.y = xy_data.y - (a + xy_data.x * b)
 
 
 # plt.figure()
-endIndex = -1
+endIndex = None
 reg_data_abs, _ = weightedLinReg(WeightedMeasurements(lunghezze[0:endIndex], periodi[0:endIndex], dev_std_lunghezze[0:endIndex], dev_std_periodi[0:endIndex]))
 
+index = 0
 x = np.linspace(lunghezze[0], lunghezze[-1], 10)
 # plt.plot(x, reg_data.a + reg_data.b * x)
 print(reg_data.chi2, reg_data.nu)
 for i in range(1, len(periodi) - 5):
     reg_data, _ = weightedLinReg(WeightedMeasurements(lunghezze[i:endIndex], periodi[i:endIndex], dev_std_lunghezze[i:endIndex], dev_std_periodi[i:endIndex]))
-    valido = reg_data.chi2 / reg_data.nu < 1.2
+    valido = reg_data.chi2 / reg_data.nu < chi2_valido
     style = 'b' if valido else 'r'
-    print(reg_data.chi2, reg_data.nu, "Valido: ", valido)
+    print(reg_data.chi2, reg_data.nu, reg_data.chi2 / reg_data.nu, "Valido:", valido)
     if valido:
         # plt.plot(x, reg_data.a + reg_data.b * x - (reg_data_abs.a + reg_data_abs.b * x), style)
         index = i
@@ -70,8 +119,12 @@ for i in range(1, len(periodi) - 5):
 # plt.errorbar(lunghezze, periodi - (reg_data_abs.a + reg_data_abs.b * lunghezze), dev_std_periodi, dev_std_lunghezze, 'kx')
 # plt.show()
 
+dev_std_effettiva = np.sqrt(dev_std_periodi**2 + (reg_data.b * dev_std_lunghezze)**2)
+
 plt.figure()
 plt.hlines(0, x[0], x[-1], 'b')
-plt.errorbar(lunghezze[index:-1], periodi[index:-1] - (reg_data.a + reg_data.b * lunghezze[index:-1]), dev_std_periodi[index:-1], dev_std_lunghezze[index:-1], 'gx')
-plt.errorbar(lunghezze[0:index], periodi[0:index] - (reg_data.a + reg_data.b * lunghezze[0:index]), dev_std_periodi[0:index], dev_std_lunghezze[0:index], 'rx')
+# plt.errorbar(lunghezze, np.log(2 * np.pi / omegas[indices[::-1]]) - (reg_data.a + reg_data.b * lunghezze), dev_std_periodi, dev_std_lunghezze, 'bx')
+
+plt.errorbar(lunghezze[index:-1], periodi[index:-1] - (reg_data.a + reg_data.b * lunghezze[index:-1]), yerr=dev_std_effettiva[index:-1], fmt='gx')
+plt.errorbar(lunghezze[0:index], periodi[0:index] - (reg_data.a + reg_data.b * lunghezze[0:index]), yerr=dev_std_effettiva[0:index], fmt='rx')
 plt.show()
